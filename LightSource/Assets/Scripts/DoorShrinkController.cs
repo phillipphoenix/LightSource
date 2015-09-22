63857140d4dcd5b2ac0e7da96ed9881c15cbba98
Oscar Losada <oscarlosu@gmail.com>
2015-09-22 16:51:32 +0200

    Door (object, animation, sound effect and level loading) and toybox level template.

---
+++ LightSource/Assets/Scripts/DoorShrinkController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorShrinkController : MonoBehaviour {
+
+    public float startingDistance = 100;
+    public float endingDistance = 10;
+    public Vector3 startScale = new Vector3(100, 100, 100);
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float dist = Vector3.Distance(transform.position, player.transform.position);
+        if(dist >= startingDistance)
+        {
+            transform.localScale = Vector3.one + startScale;
+        }
+        else if (dist < endingDistance)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else
+        {
+            float frac = Mathf.Abs(dist - endingDistance) / Mathf.Abs(startingDistance - endingDistance);
+            transform.localScale = Vector3.one + startScale * Mathf.Pow(frac, 2);
+        }
+    }
+}
--- LightSource/Assets/Scripts/SpotlightController.cs
+++ LightSource/Assets/Scripts/SpotlightController.cs
@@ -1,20 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SpotlightController : MonoBehaviour {
+public class SpotlightController : MonoBehaviour
+{
     public Light spotlight;
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    if(Input.GetKeyDown(KeyCode.R)) {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
             spotlight.color = Color.red;
-        } else if (Input.GetKeyDown(KeyCode.G)) {
+        }
+        else if (Input.GetKeyDown(KeyCode.G))
+        {
             spotlight.color = Color.green;
-        } else if (Input.GetKeyDown(KeyCode.B)) {
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
             spotlight.color = Color.blue;
         }
     }
