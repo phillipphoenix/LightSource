5c992ba23955d97fb9e20c1652f2a655ad040fb0
Oscar Losada <oscarlosu@gmail.com>
2015-12-22 19:32:19 +0100

    Folder structure was broken?

---
+++ Assets/Scripts/SpotlightController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpotlightController : MonoBehaviour
+{
+    public Light spotlight;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            spotlight.color = Color.red;
+        }
+        else if (Input.GetKeyDown(KeyCode.G))
+        {
+            spotlight.color = Color.green;
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            spotlight.color = Color.blue;
+        }
+    }
+}
